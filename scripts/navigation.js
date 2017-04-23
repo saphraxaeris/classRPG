493a2d28d5c84acb67c9e889782c2ada78fb20bf
Stephan Elias elias <Stephan Elias elias>
2017-04-23 17:56:51 -0400

    created login page

---
+++ scripts/navigation.js
@@ -0,0 +1,69 @@
+var Navigation = function() {
+    var siteUrl = "http://localhost:8080/";
+    var cookieExpiration = 3;
+    var cookieName = "classRPG-user";
+
+    var showFailedPopup = function(message) {
+        Materialize.toast(message, 3000, 'red-bg')
+    };
+
+	var handleLogin = function(){
+		$('#login-btn').on('click', function(e){
+			e.preventDefault();
+
+			var data = { usernaame: $('#username').val(), password: $('#password').val()};
+			$.ajax({
+            type: "POST",
+            url: siteUrl + "user/login",
+            dataType: "json",
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            success: function(data){
+                //set cookie
+                Cookies.set(cookieName, data, { expires: cookieExpiration});
+            	window.location.replace(siteUrl);
+			},
+            error : function() {
+                showFailedPopup("Either email or password was incorrect.");
+                $('#login-form')[0].reset();
+            }
+            });
+		});
+	};
+
+    var replaceLoginStuff = function() {
+        //Remove login/register buttons
+        var html = "<li><a href='/user/profile'><img class='profile-pic thumbnail right circle' src='http://kingofwallpapers.com/picture/picture-018.jpg'/>"+ JSON.parse(Cookies.get(cookieName)).username +"</a></li>";
+        $('.user-info').html(html);
+    };
+
+	return {
+        InitLogin: function () {
+            //verifyLogin();
+        	handleLogin();
+        },
+        ForceLogin : function() {
+            if(!Cookies.get(cookieName))
+            {
+                //Not Logged in
+                Cookies.set('error', {message: "You must be logged in to view that page."}, {expires: 1})         
+                window.location = siteUrl;
+            }
+            replaceLoginStuff();
+        },
+        VerifyLogin: function() {
+            if(Cookies.get(cookieName))
+            {
+                replaceLoginStuff();
+            }
+        },
+        VerifyError: function() {
+            if(Cookies.get('error'))
+            {
+                var text = JSON.parse(Cookies.get('error')).message;
+                Materialize.toast(text, 3000, 'red-bg')
+                Cookies.remove('error');
+            }
+        }
+    }
+}();
--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var home = require('./controller/home');
-var bodyPaser = require('bodyParser');
+var bodyParser = require('body-parser');
+var path = require('path');
 
 var app = express();
 app.use(bodyParser.json());
-app.use(express.static('view'));
+//app.use(express.static('view'));
 app.use(express.static('assets'));
 app.use(express.static('styles'));
 app.use(express.static('scripts'));
 
 app.get('/',home.index)
+
+app.get('/test', function(req,res){
+    res.sendFile(path.join(__dirname + '/view/home/index.html'));
+});
+
+app.get('/test2', function(req,res){
+    res.sendFile(path.join(__dirname + '/view/user/login.html'));
+});
+
 
 app.listen(8080);
