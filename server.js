fa8f1706c01ac0bb21a5b994c0b7f7645d22f741
Stephan Elias elias <Stephan Elias elias>
2017-04-23 14:34:20 -0400

    added a few lines

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var bodyPaser = require('bodyParser');
 var app = express();
 app.use(bodyParser.json());
 app.use(express.static('view'));
+app.use(express.static('assets'));
+app.use(express.static('styles'));
+app.use(express.static('scripts'));
 
 app.get('/',home.index)
 
